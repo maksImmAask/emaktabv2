7fe1bff70d6eb7ba1ebe64915f234a31b0647fde
Maksim Azamatov <madina@McBook-52.local>
2026-07-22 20:03:05 +0500

    scalar

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_filters',
+    
+    'django_scalar',
     # apps
     'accounts',
     'main',
--- config/urls.py
+++ config/urls.py
@@ -2,34 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import (
     SpectacularAPIView,
-    SpectacularSwaggerView,
-    SpectacularRedocView,
 )
+
+from django_scalar.views import scalar_viewer 
 urlpatterns = [
     path("admin/", admin.site.urls),
 
     path("api/auth/", include("accounts.urls")),
 
     path("api/", include("main.urls")),
-    path(
-        "api/schema/",
-        SpectacularAPIView.as_view(),
-        name="schema",
-    ),
 
-    path(
-        "api/docs/",
-        SpectacularSwaggerView.as_view(
-            url_name="schema"
-        ),
-        name="swagger-ui",
-    ),
-
-    path(
-        "api/redoc/",
-        SpectacularRedocView.as_view(
-            url_name="schema"
-        ),
-        name="redoc",
-    ),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('scalar/', scalar_viewer, name='scalar-viewer'),
+    
 ]
